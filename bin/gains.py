25ec1813a196ff857dd04533ee9a2eb3e64fc0a3
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2019-10-13 10:15:30 -0600

    Fix the calculation.
    Add debugging.
    Permit specifying base and snippet directories.

--- bin/gains.py
+++ bin/gains.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """How much can snippet compression learn from different bases?"""
+# pylint: disable=too-many-locals
 
 from collections import namedtuple
 from collections import defaultdict
@@ -11,9 +12,6 @@ import parseargs
 import sizes
 from sizes import Sizes
 
-BASE_DIR = "ot.d"
-SNIPPET_DIR = "nt.d"
-
 
 Chunk = namedtuple("Chunk", "name, text")
 
@@ -21,17 +19,30 @@ Chunk = namedtuple("Chunk", "name, text")
 def taught_size(base, snippet, base_sizes):
     """Size increase from adding snippet, then compressing."""
     combined_size = len(zlib.compress(base.text + snippet.text))
-    return combined_size - base_sizes[base.name]
+    compressed_snippet_size = combined_size - base_sizes[base.name]
+    logging.debug(
+        (
+            "base: %s, snippet: %s, base_size: %s, "
+            "combined_size: %s, compressed_snippet_size: %s"
+        ),
+        base.name,
+        snippet.name,
+        base_sizes[base.name],
+        combined_size,
+        compressed_snippet_size,
+    )
+    return compressed_snippet_size
 
 
 def main():
     """The feature attraction."""
     args = parseargs.parseargs()
-    logging.basicConfig(level=(logging.DEBUG if args.debug else logging.INFO))
+    base_dir = args.base_dir
+    snippet_dir = args.snippet_dir
 
-    base_names = sorted([file for file in os.listdir(BASE_DIR) if sizes.not_json(file)])
+    base_names = sorted([file for file in os.listdir(base_dir) if sizes.not_json(file)])
     snippet_names = sorted(
-        [file for file in os.listdir(SNIPPET_DIR) if sizes.not_json(file)]
+        [file for file in os.listdir(snippet_dir) if sizes.not_json(file)]
     )
 
     logging.debug("base_names: %s", str(base_names))
@@ -43,11 +54,11 @@ def main():
     taught_sizes = defaultdict(dict)
 
     for base_name in base_names:
-        base_sizes = Sizes(BASE_DIR).sizes()
-        with open(os.path.join(BASE_DIR, base_name), "rb") as bfd:
+        base_sizes = Sizes(base_dir).sizes()
+        with open(os.path.join(base_dir, base_name), "rb") as bfd:
             base = Chunk(name=base_name, text=bfd.read())
         for snippet_name in snippet_names:
-            with open(os.path.join(SNIPPET_DIR, snippet_name), "rb") as sfd:
+            with open(os.path.join(snippet_dir, snippet_name), "rb") as sfd:
                 snippet = Chunk(name=snippet_name, text=sfd.read())
             taught_sizes[base_name][snippet_name] = taught_size(
                 base, snippet, base_sizes
@@ -55,9 +66,16 @@ def main():
 
     for base_name in base_names:
         row = [base_name]
-        self_size = taught_sizes[base_name][base_name]
         for snippet_name in snippet_names:
-            row.append(str(taught_sizes[base_name][snippet_name] - self_size))
+            self_size = taught_sizes[snippet_name][snippet_name]
+            logging.debug(
+                "%s  %s %s %s",
+                base_name,
+                snippet_name,
+                taught_sizes[base_name][snippet_name],
+                self_size,
+            )
+            row.append(str(self_size - taught_sizes[base_name][snippet_name]))
         print(",".join(row))
 
 
--- bin/parseargs.py
+++ bin/parseargs.py
@@ -3,13 +3,30 @@
 """Parse the args"""
 
 import argparse
+import logging
+
+BASE_DIR = "ot.d"
+SNIPPET_DIR = "nt.d"
 
 
 def parseargs():
     """Parse arguments."""
     parser = argparse.ArgumentParser(description="Show compression gains.")
     parser.add_argument("--debug", action="store_true", help="Turn on debugging")
-    return parser.parse_args()
+    parser.add_argument(
+        "--base_dir", default=BASE_DIR, help="directory of training bases"
+    )
+    parser.add_argument(
+        "--snippet_dir", default=SNIPPET_DIR, help="directory of snippets to compress"
+    )
+    args = parser.parse_args()
+
+    # turn on logging
+    logging.basicConfig(level=(logging.DEBUG if args.debug else logging.INFO))
+    logging.debug("base_dir: %s", args.base_dir)
+    logging.debug("snippet_dir: %s", args.snippet_dir)
+
+    return args
 
 
 if __name__ == "__main__":
